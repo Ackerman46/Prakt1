1bb9c9f4bcc7956fb55bbcb16e9234bcebb56e2a
ИСП-31 <-31@LAB17-12>
2022-10-27 13:01:54 +0300

    Добавьте файлы проекта.

---
+++ Практическая 1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Lib_12;
+
+namespace Prakt1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            userN.Focus();
+        }
+
+        private void CloseApp_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void TaskApp_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Агеев ИСП-31\nВариант 12\nЗадание: Найти сумму n целых, случайных чисел, распределенных в диапазоне от 0 до n.\nВывести на экран на одной строке сгенерированные числа, на другой строке результат");
+        }
+
+        private void userN_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (userN.Text != "")
+            {
+                int.TryParse(userN.Text, out int n);
+                resultBox.Text = $"Сумма: {LibArr.SumOfNumbers(out string genNumbers, n)}\n" + genNumbers;
+            }
+            else
+            {
+                resultBox.Clear();
+            }
+        }
+    }
+}
